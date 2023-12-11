d4dd33011eac31460f5134dbf987dea6ee73f5f2
Aurasj <thelimeyt14@gmail.com>
2023-12-11 18:43:17 +0200

    C#UnityCodes

---
+++ UnityC#App/BluetoothService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using UnityEngine;
+public class BluetoothService
+{
+    private static AndroidJavaClass unityPlayer;
+    private static AndroidJavaObject activity;
+    private static AndroidJavaObject context;
+    private static AndroidJavaClass unity3dbluetoothplugin;
+    private static AndroidJavaObject BluetoothConnector;
+ 
+    public static void CreateBluetoothObject()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            context = activity.Call<AndroidJavaObject>("getApplicationContext");
+            unity3dbluetoothplugin = new AndroidJavaClass("com.example.unity3dbluetoothplugin.BluetoothConnector");
+            BluetoothConnector = unity3dbluetoothplugin.CallStatic<AndroidJavaObject>("getInstance");
+        }
+    }
+
+    public static string[] GetBluetoothDevices()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            try
+            {
+                return BluetoothConnector.Call<string[]>("GetBluetoothDevices");
+            }
+            catch (Exception e)
+            {
+                Toast("No Device found");
+                return null;
+            }
+        }
+
+        return null;
+
+    }
+
+    public static bool StartBluetoothConnection(string DeviceName)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            try
+            {
+                string connectionStatus = BluetoothConnector.Call<string>("StartBluetoothConnection", DeviceName);
+                Toast("Start connection status: " + connectionStatus);
+                if (connectionStatus == "Connected") 
+                    return true;
+
+            }
+            catch (Exception e)
+            {
+                Toast("Start connection error");
+            }
+        }
+
+        return false;
+    }
+
+    public static void StopBluetoothConnection()
+    {
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            try
+            {
+                BluetoothConnector.Call("StopBluetoothConnection");
+                Toast("Connction stoped");
+
+            }
+            catch (Exception e)
+            {
+                Toast("Stop connction error");
+            }
+        }
+    }
+
+    public static void WritetoBluetooth(string data)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            try
+            {
+                BluetoothConnector.Call("WriteData", data);
+            }
+            catch (Exception e)
+            {
+                Toast("Write data error");
+            }
+        }
+    }
+
+    public static string ReadFromBluetooth()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            try
+            {
+                return BluetoothConnector.Call<string>("ReadData");
+            }
+            catch (Exception e)
+            {
+                BluetoothConnector.Call("PrintOnScreen", context, "Read data error");
+            }
+        }
+         
+        return "";
+
+    }
+
+    public static void Toast(string data)
+    {
+        BluetoothConnector.Call("PrintOnScreen", context, data);
+        Debug.Log(data);
+    }
+
+}
---
+++ UnityC#App/BluetoothTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class BluetoothTest : MonoBehaviour
+{
+    public Text deviceName;
+    public Text dataToSend;
+    private bool IsConnected;
+    public static string dataRecived = "";
+    public GameObject startCarPanel;
+    public GameObject controllerPanel;
+    public TMP_Text isConnectText;
+
+    void Start()
+    {
+#if UNITY_2020_2_OR_NEWER
+#if UNITY_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.CoarseLocation)
+          || !Permission.HasUserAuthorizedPermission(Permission.FineLocation)
+          || !Permission.HasUserAuthorizedPermission("android.permission.BLUETOOTH_SCAN")
+          || !Permission.HasUserAuthorizedPermission("android.permission.BLUETOOTH_ADVERTISE")
+          || !Permission.HasUserAuthorizedPermission("android.permission.BLUETOOTH_CONNECT"))
+            Permission.RequestUserPermissions(new string[] {
+                        Permission.CoarseLocation,
+                            Permission.FineLocation,
+                            "android.permission.BLUETOOTH_SCAN",
+                            "android.permission.BLUETOOTH_ADVERTISE",
+                             "android.permission.BLUETOOTH_CONNECT"
+                    });
+#endif
+#endif
+
+        IsConnected = false;
+        BluetoothService.CreateBluetoothObject();
+
+    }
+    void Update()
+    {
+        if (IsConnected)
+        {
+            try
+            {
+                string datain = BluetoothService.ReadFromBluetooth();
+                if (datain.Length > 1)
+                {
+                    dataRecived = datain;
+                    print(dataRecived);
+                }
+
+            }
+            catch (Exception e)
+            {
+                BluetoothService.Toast("Error in connection");
+            }
+            isConnectText.text = "Pornita";
+            isConnectText.color = Color.green;
+        }
+        else
+        {
+            isConnectText.text = "Oprita";
+            isConnectText.color = Color.red;
+        }
+    }
+
+    //Connection
+    public void GetDevicesButton()
+    {
+        string[] devices = BluetoothService.GetBluetoothDevices();
+
+        foreach (var d in devices)
+        {
+            Debug.Log(d);
+        }
+    }
+    public void StartButton()
+    {
+        if (!IsConnected)
+        {
+            IsConnected = BluetoothService.StartBluetoothConnection(deviceName.text.ToString());
+            BluetoothService.Toast(deviceName.text.ToString() + " status: " + IsConnected);
+        }
+        if (IsConnected)
+        {
+            startCarPanel.gameObject.SetActive(false);
+            controllerPanel.gameObject.SetActive(true);
+        }
+    }
+    public void SendButton()
+    {
+        if (IsConnected && (dataToSend.ToString() != "" || dataToSend.ToString() != null))
+            BluetoothService.WritetoBluetooth(dataToSend.text.ToString());
+        else
+            BluetoothService.WritetoBluetooth("Not connected");
+    }
+    public void StopButton()
+    {
+        if (IsConnected)
+        {
+            BluetoothService.StopBluetoothConnection();
+        }
+    }
+
+    //Controls
+    public void UpEvent()
+    {
+        BluetoothService.WritetoBluetooth("S");
+        Debug.Log("Send null");
+    }
+    public void ControllUp()
+    {
+        BluetoothService.WritetoBluetooth("F");
+        Debug.Log("F");
+    }
+    public void ControllDown()
+    {
+        BluetoothService.WritetoBluetooth("B");
+        Debug.Log("B");
+
+    }
+    public void ControllRight()
+    {
+        BluetoothService.WritetoBluetooth("R");
+        Debug.Log("R");
+
+    }
+    public void ControllLeft()
+    {
+        BluetoothService.WritetoBluetooth("L");
+        Debug.Log("L");
+
+    }
+
+    //Music
+    public void MusicOn()
+    {
+        BluetoothService.WritetoBluetooth("M");
+        Debug.Log("M");
+    }
+    public void MusicOff()
+    {
+        BluetoothService.WritetoBluetooth("N");
+        Debug.Log("N");
+    }
+}
